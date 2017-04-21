d43961d8e3660cbd5afb14e263ef9a39a5af94e0
Joan Miralles <joan.miralles@gmail.com>
2017-04-21 14:00:47 +0200

    Move to rulz dependency

--- hello/src/test/java/com/airhacks/hello/business/order/entity/OrderIT.java
+++ hello/src/test/java/com/airhacks/hello/business/order/entity/OrderIT.java
@@ -1,18 +1,19 @@
 package com.airhacks.hello.business.order.entity;
 
+import com.airhacks.rulz.em.EntityManagerProvider;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class OrderIT {
 
     @Rule
-    public EntityManagerProvider provider = EntityManagerProvider.withUnit("it");
+    public EntityManagerProvider provider = EntityManagerProvider.persistenceUnit("it");
 
     @Test
     public void verifyMappings() {
-        this.provider.begin();
+        this.provider.tx().begin();
         this.provider.em().merge(new Order("42"));
-        this.provider.commit();
+        this.provider.tx().commit();
     }
 
 }
