85c732baf8e7bd05958a866433b008d3f49e8829
Joan Miralles <joan.miralles@gmail.com>
2017-04-28 17:21:55 +0200

    Add new 2 projects monitoring and monitoring-st to show how System Tests work

---
+++ monitoring/src/main/java/com/airhacks/mon/business/reporting/boundary/SnapshotsResource.java
@@ -0,0 +1,16 @@
+package com.airhacks.mon.business.reporting.boundary;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("snapshots")
+public class SnapshotsResource {
+    @GET
+    public JsonObject snapshots() {
+        return Json.createObjectBuilder().
+                add("s1", "fast").
+                build();
+    }
+}
