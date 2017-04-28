b80d7c701568751e4bd04354c4ecfd7e8ab21d78
Joan Miralles <joan.miralles@gmail.com>
2017-04-28 17:57:12 +0200

    Add IT Test using jersey-test-framework

---
+++ monitoring/src/test/java/com/airhacks/mon/business/reporting/boundary/SnapshotsResourceIT.java
@@ -0,0 +1,29 @@
+package com.airhacks.mon.business.reporting.boundary;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.json.JsonObject;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SnapshotsResourceIT extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(SnapshotsResource.class);
+    }
+
+    @Test
+    public void crud() {
+        Response response = target("snapshots").request().get();
+        assertThat(response.getStatus(), is(200));
+        JsonObject result = response.readEntity(JsonObject.class);
+        System.out.println("result = " + result);
+    }
+
+}
