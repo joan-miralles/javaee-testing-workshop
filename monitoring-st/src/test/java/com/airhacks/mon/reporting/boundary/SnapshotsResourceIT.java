c2b914cf31df51e510e109f6ba9510c409919d4a
Joan Miralles <joan.miralles@gmail.com>
2017-04-28 17:56:35 +0200

    Add IT Test using jersey-test-framework

--- monitoring-st/src/test/java/com/airhacks/mon/reporting/boundary/SnapshotsResourceIT.java
+++ monitoring-st/src/test/java/com/airhacks/mon/reporting/boundary/SnapshotsResourceIT.java
@@ -1,5 +1,6 @@
 package com.airhacks.mon.reporting.boundary;
 
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -7,19 +8,19 @@ import javax.json.JsonObject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static org.hamcrest.CoreMatchers.is;
+import static com.airhacks.rulz.jaxrsclient.HttpMatchers.successful;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 public class SnapshotsResourceIT {
 
     @Rule
-    public JAXRSClient provider = JAXRSClient.target("http://localhost:8080/monitoring/resources/snapshots");
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:8080/monitoring/resources/snapshots");
 
     @Test
     public void snapshots() {
-        Response response = this.provider.tut().request(MediaType.APPLICATION_JSON).get();
-        assertThat(response.getStatus(), is(200));
+        Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
+        assertThat(response, successful());
         JsonObject result = response.readEntity(JsonObject.class);
         assertNotNull(result);
         System.out.println("result = " + result);
